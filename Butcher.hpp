869e1fce3a666c5375a35fcd34a59d550bcc4a16
James Meehan <ghoyas2003@gmail.com>
2018-06-14 22:55:06 -0400

    Update Butcher.hpp

--- Butcher.hpp
+++ Butcher.hpp
@@ -1,7 +1,5 @@
 /*********************************************
  * Program Name: outlaw
- * Author Daniel Yopp
- * Date: 6/6/18
  * Description: Butcher class declarations. Butcher class is a child class from space class and repersents the functions for the butchershop in town.  
  * ******************************************/
 
