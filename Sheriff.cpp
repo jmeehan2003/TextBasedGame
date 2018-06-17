53eedcb1726412031deacb5a8ea318545cc06b45
James Meehan <ghoyas2003@gmail.com>
2018-06-17 13:37:48 -0400

    Update Sheriff.cpp

--- Sheriff.cpp
+++ Sheriff.cpp
@@ -1,7 +1,6 @@
 /******************************************************************
  * Program Name: outlaw
- * Author:Daniel Yopp
- * Date: 6/7/18
+ * Author: James Meehan
  * Description: Sheriff class member functions
  * ***************************************************************/
 #include <iostream>
